01c510c9e58b7a9235c5f2bb82bf234982dabbb1
Dominik Skála <DominikSkala@seznam.cz>
2019-03-26 20:14:51 +0100

    add metatags and app

--- src/index.js
+++ src/index.js
@@ -14,7 +14,20 @@ import {MetaTags, ReactTitle} from 'react-meta-tags';
 document.title=config.pageName;
 
 ReactDOM.render(
-    "Hello world"
+	<BrowserRouter>
+	<>
+        <ReactTitle title={config.pageName} />
+        <MetaTags>
+            <meta charSet="utf-8" />
+            <title>{config.pageName}</title>
+            <meta name="description" content="Hledáte svůj vysněný herní server? Pak stačí hledat zde! ServerList Vám pomůže najít místo ke hraní!" />
+            <meta property="og:title" content={config.pageName} />
+            <meta property="keywords" content="minecraft, counter-ctrike: global offensive, rust, csgo, cs:go, mc, server, serverlist, list, hry, servery"/>
+            <meta name="robots" content="index,follow"/>
+        </MetaTags>
+        <App/>
+	</>
+	</BrowserRouter>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
