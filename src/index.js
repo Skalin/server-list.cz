fb2e15a978d249234369d7a96adf7035f448a169
Dominik Skála <DominikSkala@seznam.cz>
2019-03-26 10:34:40 +0100

    updated seo

--- src/index.js
+++ src/index.js
@@ -8,6 +8,32 @@ import * as config from './config/config.js';
 
 import {MetaTags, ReactTitle} from 'react-meta-tags';
 
+window.onerror = function (message, url, lineNo, colNo, error) {
+
+    console.log(arguments);
+
+    let container = document.createElement('div');
+
+    container.style.color = 'red';
+    container.style.position = 'fixed';
+    container.style.background = '#eee';
+    container.style.padding = '2em';
+    container.style.top = '1em';
+    container.style.left = '1em';
+
+    let msg = document.createElement('pre');
+    msg.innerText = [
+        'Message: ' + message,
+        'URL: ' + url,
+        'Line: ' + lineNo,
+        'Column: ' + colNo,
+        'Stack: ' + (error && error.stack)
+    ].join('\n');
+
+    container.appendChild(msg);
+
+    document.body.appendChild(container);
+};
 
 document.title=config.pageName;
 
