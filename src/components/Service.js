1125ffbe811c8579322072f290feaf360b642bdb
Dominik Skála <DominikSkala@seznam.cz>
2019-04-23 18:40:46 +0200

    updated layout

--- src/components/Service.js
+++ src/components/Service.js
@@ -43,7 +43,7 @@ const styles = theme => ({
         flexDirection: 'column',
     },
     cardMedia: {
-        paddingTop: '56.25%', // 16:9
+        backgroundColor: "#55595c"
     },
     cardContent: {
         flexGrow: 1,
@@ -65,6 +65,32 @@ class Services extends Component
     componentDidMount() {
         axios.get(this.endpoint)
             .then(res => this.setState({services: res.data}));
+    }
+
+    renderBackgroundCardImage( service )
+    {
+        const { classes } = this.props;
+        if (service.imageUrl)
+        {
+            return (
+
+                <CardMedia
+                    className={classes.cardMedia} title={service.name}>
+                    <img src={service.thumbnailImageUrl} height={200} />
+                </CardMedia>
+            );
+        }
+        else
+        {
+            return (
+                <CardMedia
+                    className={classes.cardMedia}
+                    title={service.name}
+                >
+                    <img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+                         />
+                </CardMedia>)
+        }
     }
 
     render() {
@@ -90,7 +116,7 @@ class Services extends Component
                                 <Grid item xs={12} sm={6} md={4} key={service.id}>
                                     <Link to={{pathname: '/services/'+service.id, state: {service: service}}}>
                                         <Card className={classes.card}>
-                                            <CardMedia/>
+                                            {this.renderBackgroundCardImage(service)}
                                             <CardContent>
                                                 <Typography component={"h5"}>
                                                     {service.name}
