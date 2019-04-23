ffad7500a7d0c59ba954780c3d619ec4a9cd4013
Dominik Skála <DominikSkala@seznam.cz>
2019-04-23 15:30:08 +0200

    updated layout

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,8 +1,9 @@
 import  React from 'react';
 import '../App.css'
 import {Link} from 'react-router-dom';
-import { Navbar, Nav } from "react-bootstrap";
-import NavLink from "react-bootstrap/NavLink";
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import {Grid, AppBar, Toolbar} from '@material-ui/core';
 
 
 const styles = {
@@ -14,11 +15,11 @@ const styles = {
 function Footer() {
 
     return (
-        <Navbar fixed={"bottom"}>
-            <Nav>
-                <Link style={styles.link} to="/conditions">Podmínky</Link>
-            </Nav>
-        </Navbar>
+        <Grid container justify={"center"} alignItems={"center"} alignContent={"center"}>
+            <Grid item xs={12}>
+                    <Link style={styles.link} to="/conditions">Podmínky</Link>
+            </Grid>
+        </Grid>
     )
 }
 export default Footer;
