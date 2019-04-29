509cde78369c27bae89413857d8ee7c8d15396f3
Dominik Skála <dominikskala@seznam.cz>
2019-04-29 15:10:06 +0200

    updated colors

--- src/layout/Navigation.js
+++ src/layout/Navigation.js
@@ -32,12 +32,13 @@ const styles = {
         marginRight: 20,
         '&:hover': {
             textDecoration: "none",
-            color: 'grey',
+            color: "rgb(72,72,72)",
         },
     },
     heading: {
         backgroundColor: "rgba(0, 120, 255, 1)",
         color: 'white',
+        marginBottom: "2em",
     },
     link: {
         color: 'white',
