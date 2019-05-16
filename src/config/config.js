da317058e22c7d3f10234e59359740dfec940d73
Dominik Skála <DominikSkala@seznam.cz>
2019-05-16 15:02:17 +0200

    added config

---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+
+export const apiUrl = 'https://api.server-list.cz/v1/';
+
+export const apiUserUrl = apiUrl+'user';
+
+export const pageName = "ServerList";
+export const titlePageName = " | "+ pageName;
